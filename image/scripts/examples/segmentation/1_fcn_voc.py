12a3108eb9cf85cbb34605117027f281475d6256
knakamura <c60evaporator@gmail.com>
2024-04-17 21:26:23 -0700

    Small modification

--- image/scripts/examples/segmentation/1_fcn_voc.py
+++ image/scripts/examples/segmentation/1_fcn_voc.py
@@ -58,13 +58,16 @@ if DEVICE == 'cpu':
 torch.manual_seed(SEED)
 
 ###### 1. Create dataset & Preprocessing ######
-# Define preprocessing of the image (https://poutyne.org/examples/semantic_segmentation.html)
+# Define preprocessing of the image
+def collate_fn(batch):  # collate_fn is needed if the sizes of resized images are different
+    return tuple(zip(*batch))
+# Reference resize_size=520 (https://pytorch.org/vision/main/models/generated/torchvision.models.segmentation.fcn_resnet50.html)
 transform = transforms.Compose([
     transforms.Resize((224, 224)),  # Resize the image to 224px x 224px
     transforms.ToTensor(),  # Convert from range [0, 255] to a torch.FloatTensor in the range [0.0, 1.0]
     transforms.Normalize(IMAGENET_MEAN, IMAGENET_STD)  # Normalization (mean and std of the imagenet dataset for normalizing)
 ])
-# Define preprocessing of the target for VOCSegmentation dataset
+# Define preprocessing of the target for VOCSegmentation dataset (https://poutyne.org/examples/semantic_segmentation.html)
 def replace_tensor_value_(tensor, a, border_class):
     tensor[tensor == a] = border_class
     return tensor
